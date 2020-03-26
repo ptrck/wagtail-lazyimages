2e7f919bfbe6f852aafd3ca5b22ff5998f600f79
Patrick Dengler <info@patrickdengler.de>
2020-03-26 03:06:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtail-lazyimages',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
